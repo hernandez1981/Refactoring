b2c0414af8d9cf6547e5c9467565278e5c8d57d6
hernandez1981 <kathya_lucia@hotmail.com>
2022-05-16 00:03:57 -0600

    Creando directorio y archivo ExplorerService junto con prueba de unidad

---
+++ test/services/ExplorerService.test.js
@@ -0,0 +1,22 @@
+const Reader = require("./../../lib/utils/Reader");
+const ExplorerService = require("./../../lib/services/ExplorerService");
+
+describe("Unit Tests for ExplorerService class", () => {
+        
+    const explorers = Reader.readJsonFile("explorers.json");   
+
+    test('Get list of explorersByMission in node ', () => {
+        const result = ExplorerService.filterByMission(explorers, "node");
+        expect( result[0].mission ).toBe("node");         
+    });
+
+    // test('Get list of getAmountOfExplorersByMission', () => {              
+    //     const explorersInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+    //     expect(explorersInNode.length).toBe(10);          
+    // });
+
+    // test('Get list of Usernames By Mission', () => {        
+    //     const usernamesInNode  = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+    //     expect(usernamesInNode).toContain('ajolonauta2');         
+    // });    
+});
