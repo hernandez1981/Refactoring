0ae4b0ab21d86473c2790baf451cfdb550291bf4
hernandez1981 <kathya_lucia@hotmail.com>
2022-05-18 22:48:50 -0600

    Agregando prueba de unidad de la clase ExplorerController

---
+++ test/controller/ExplorerController.test.js
@@ -0,0 +1,19 @@
+const ExplorerController = require("./../../lib/controllers/ExplorerController");
+
+describe("Unit Tests for ExplorerController class", () => {     
+
+    test("Get list of Explorers by Mission ", () => {
+        const explorers = ExplorerController.getExplorersByMission("node");
+        expect( explorers[0].mission ).toBe("node");         
+    });
+
+    test("Get list of Usernames By Mission", () => {        
+        const usernamesInNode = ExplorerController.getExplorersUsernamesByMission("node");
+        expect(usernamesInNode).toContain("ajolonauta3");         
+    });  
+
+    test("Get amount of explorers in node", () => {              
+        const explorersInNode = ExplorerController.getExplorersAmountByMission("node");
+        expect(explorersInNode.length).toBe(10);          
+    });       
+});
