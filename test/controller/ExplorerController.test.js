4c7399223995fb800a6e1474ee915b7d25e4568b
hernandez1981 <kathya_lucia@hotmail.com>
2022-05-24 23:37:08 -0600

    Agregando nuevo método getValidationInNumber en ExplorerController junto con su prueba unitaria

--- test/controller/ExplorerController.test.js
+++ test/controller/ExplorerController.test.js
@@ -16,4 +16,16 @@ describe("Unit Tests for ExplorerController class", () => {
         const explorersInNode = ExplorerController.getExplorersAmountByMission("node");
         expect(explorersInNode).toBe(10);          
     });       
+
+    test("New validation in number get trick value", () => {              
+        const score1 = ExplorerController.getValidationInNumber(1);
+        const score3 = ExplorerController.getValidationInNumber(3);
+        const score5 = ExplorerController.getValidationInNumber(5);
+        const score15 = ExplorerController.getValidationInNumber(15);
+        expect(score1).toBe(1);   
+        expect(score3).toBe("FIZZ");   
+        expect(score5).toBe("BUZZ");   
+        expect(score15).toBe("FIZZBUZZ");  
+    }); 
+
 });
